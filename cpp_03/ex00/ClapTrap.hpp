9ff920eb5adecc922dd4dc38b22b0a9a66fa6d83
Kenan GEZGIN <kgezgin@made-f0Ar11s9.clusters.42paris.fr>
2023-10-20 10:36:06 +0200

    first push 20/10

--- cpp_03/ex00/ClapTrap.hpp
+++ cpp_03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: kgezgin <kgezgin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 10:30:16 by kgezgin           #+#    #+#             */
-/*   Updated: 2023/10/11 18:49:48 by kgezgin          ###   ########.fr       */
+/*   Updated: 2023/10/20 10:16:42 by kgezgin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ class ClapTrap
 		ClapTrap		&operator=(const ClapTrap &other);
 		void 			attack(const std::string &target);
 		void 			takeDamage(unsigned int amount);
-		void 			beRepaired(unsigned int amount);
+		void 			beRepaired(unsigned int amount);	
 
 	private:
 		std::string		name;
